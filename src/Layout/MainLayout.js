ec37c1db502685d5a17b5c971300c608eb2ee264
iamazingg <64915362+iamazingg@users.noreply.github.com>
2023-03-06 08:27:59 +0800

    Navbar

--- src/Layout/MainLayout.js
+++ src/Layout/MainLayout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../Partials/Navbar/Navbar';
 
 const MainLayout = () => {
   return (
-    <>
+    <main className="relative h-screen overflow-hidden bg-gray-100">
+      <Navbar />
       <Outlet />
-    </>
+    </main>
   );
 };
 
--- src/Partials/Navbar/Navbar.js
+++ src/Partials/Navbar/Navbar.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="w-full flex">
+      <h1>UI KIT</h1>
+    </div>
+  );
 };
 
 export default Navbar;
